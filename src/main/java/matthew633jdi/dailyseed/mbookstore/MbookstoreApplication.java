03244ae0f3549a7c69c664aaf663100678b42472
matthew633jdi <matthew633jdi@gmail.com>
2026-03-05 16:06:19 +0900

    initialize project

---
+++ src/main/java/matthew633jdi/dailyseed/mbookstore/MbookstoreApplication.java
@@ -0,0 +1,13 @@
+package matthew633jdi.dailyseed.mbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matthew633jdi/dailyseed/mbookstore/MbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package matthew633jdi.dailyseed.mbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
