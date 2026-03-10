32cc03f01348d38eedb7048902d13d4fa88d2f2b
matthew633jdi <matthew633jdi@gmail.com>
2026-03-10 23:51:15 +0900

    feat: 계층형 구조를 위한 Category entity 추가

---
+++ src/main/java/matthew633jdi/dailyseed/mbookstore/category/Category.java
@@ -0,0 +1,43 @@
+package matthew633jdi.dailyseed.mbookstore.category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children = new ArrayList<>();
+
+    public Category(String name, Category parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public void addChild(Category child) {
+        this.children.add(child);
+        child.setParent(this);
+    }
+
+    private void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+}
